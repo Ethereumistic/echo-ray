694413322c81e664102cb5be17523f346490329a
Ethereumistic <badjarovv@gmail.com>
2024-10-24 19:23:39 +0300

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  images: {
+    domains: ['assets.aceternity.com'], // Add specific domains here
+    // To allow all URLs (not recommended for production)
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**', // Allow all hostnames
+      },
+    ],
+  },
 };
 
 export default nextConfig;
